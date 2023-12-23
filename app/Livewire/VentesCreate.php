434d50dade1ad7350b0404e443be6992be2e2127
jonasdevpro <kjonasdevpro@gmail.com>
2023-12-23 17:08:53 +0100

    maj ventes create

--- app/Livewire/VentesCreate.php
+++ app/Livewire/VentesCreate.php
@@ -5,24 +5,26 @@ namespace App\Livewire;
 use DateTime;
 use App\Models\User;
 use App\Models\Pompe;
+use App\Models\Vente;
 use Livewire\Component;
 use App\Models\Configuration;
+use Illuminate\Support\Facades\Auth;
 
 class VentesCreate extends Component
 {
 
-    public $index_depart_essence;
-    public $index_arrive_essence;
+    public $index_arrive_essence = 120;
+    public $index_depart_essence = 10;
 
-    public $qte_essence;
-    public $prix_essence;
+    // public $qte_essence;
+    // public $prix_essence;
 
 
-    public $index_depart_gazoile;
-    public $index_arrive_gazoile;
+    public $index_depart_gazoile = 10;
+    public $index_arrive_gazoile = 120;
     // public $active;
     public $heureService;
-    public $chefPisteId;
+    // public $chefPisteId;
     public $pompisteId;
     public $pompeId;
     
@@ -30,50 +32,130 @@ class VentesCreate extends Component
     public $lubrifiant_vendu = 0;
     public $gaz_vendu = 0;
 
-    public $horaires;
+    public $horaires = [];
     public $liste_pompe;
-    public $configurations;
+    // public $configurations;
+    public $users;
+    public $unite_e;
+    public $unite_g;
+    // public $ecart;
+    // public $montant;
+    public $montant_recu = 0;
 
     public function mount()
-    {
-        // Récupérer les horaires depuis la table Configuration
-        $configurations = Configuration::first(); // Supposons qu'il y ait une seule entrée dans la table
-
-        if ($configurations) {
-            $heureDebutMatin = new DateTime($configurations->heure_debut_service_matin);
-            $heureReleve = new DateTime($configurations->heure_releve);
-            $heureFinSoir = new DateTime($configurations->heure_fin_service_soir);
-        
-            $this->horaires = [
-                'matin' => $heureDebutMatin->format('H:i') . ' - ' . $heureReleve->format('H:i'),
-                'soir' => $heureReleve->format('H:i') . ' - ' . $heureFinSoir->format('H:i'),
-            ];
-        } else {
-            // Valeurs par défaut si $configurations est null
-            $this->horaires = [
-                'matin' => '00:00 - 00:00',
-                'soir' => '00:00 - 00:00',
-            ];
-        }
-        
+    {  
+        $this->listePompe();
+        $this->listeEmployer();
+        $this->UnitePrix();
+        $this->Config();
+        // $this->saveVentes();  
     }
     
+    public function saveVentes()
+    {
+        // dd($this->all());
+        // $this->validate([
+        //     'heure_service' => 'required',
+        //     'index_depart_essence' => 'required|numeric',
+        //     'index_arrive_essence' => 'required|numeric',
+        //     'index_depart_gazoile' => 'required|numeric',
+        //     'index_arrive_gazoile' => 'required|numeric',
+        //     'qte_essence' => 'required|numeric',
+        //     'prix_essence' => 'required|numeric',
+        //     'prix_gazoile' => 'required|numeric',
+        //     'qte_gazoile' => 'required|numeric',
+        //     'montant' => 'required|integer',
+        //     'montant_recu' => 'required|numeric',
+        // ]);
+        
+        $prixEssence = ( $this->index_arrive_essence - $this->index_depart_essence) * $this->unite_e;
+        $prixGazoile =( $this->index_arrive_gazoile - $this->index_depart_gazoile) * $this->unite_g;
+        $montant = $prixGazoile + $prixEssence;
+        $ecart = $this->montant_recu - $montant;
+        
+        $ventes = Vente::create([
+            'chef_piste_id' => Auth::user()->id,
+            'pompiste_id' => $this->pompisteId,
+            'pompe_id' => $this->pompeId,
+            'heure_service' => $this->heureService,
+            'index_depart_essence' => $this->index_depart_essence,
+            'index_arrive_essence' => $this->index_arrive_essence,
+            'index_depart_gazoile' => $this->index_depart_gazoile,
+            'index_arrive_gazoile' => $this->index_arrive_gazoile,
+            'qte_essence' => $this->index_arrive_essence - $this->index_depart_essence,
+            'prix_essence' => $prixEssence,
+            'qte_gazoile' => $this->index_arrive_gazoile - $this->index_depart_gazoile,
+            'prix_gazoile' => $prixGazoile,
+            'montant_recu' => $this->montant_recu,
+            'montant' => $montant,
+            'ecart' => $ecart,
+        ]);
+        
+        // dd($ventes);
+        // Réinitialiser les propriétés si nécessaire
+        $this->reset([
+            'pompisteId',
+            'pompeId',
+            'heureService',
+            'index_depart_essence',
+            'index_arrive_essence',
+            'index_depart_gazoile',
+            'index_arrive_gazoile',
+            'montant_recu',
+        ]);
+    
+        // Si vous avez besoin de rediriger l'utilisateur ou de faire d'autres actions après la création
+        // return redirect()->to('chemin_vers_redirection');
+    }
+    
+
+    public function Config(){
+         // Récupérer les horaires depuis la table Configuration
+         $configurations = Configuration::first(); // Supposons qu'il y ait une seule entrée dans la table
+
+         if ($configurations) {
+             $heureDebutMatin = new DateTime($configurations->heure_debut_service_matin);
+             $heureReleve = new DateTime($configurations->heure_releve);
+             $heureFinSoir = new DateTime($configurations->heure_fin_service_soir);
+         
+             $this->horaires = [
+                 'matin' => $heureDebutMatin->format('H:i') . ' - ' . $heureReleve->format('H:i'),
+                 'soir' => $heureReleve->format('H:i') . ' - ' . $heureFinSoir->format('H:i'),
+             ];
+         } else {
+             // Valeurs par défaut si $configurations est null
+             $this->horaires = [
+                 'matin' => '00:00 - 00:00',
+                 'soir' => '00:00 - 00:00',
+             ];
+         }
+    }
+    
+    protected function UnitePrix(){
+        $this->unite_e = Configuration::first()->prix_litre_essence;
+        $this->unite_g = Configuration::first()->prix_litre_diesel;
+
+    // dd($this->unite_e, $this->unite_g);
+    }
     //recuper la lise ds pompes ici
     public function listePompe() {
         $this->liste_pompe =  Pompe::all();
     }
-    
 
-    public function saveVente()
-    {
-        
+    
+    public function listeEmployer(){
+        $this->users = User::where('role', 'pompiste')->get();
+        // dd($this->users);
     }
+   
     public function render()
     {   
-        
-        $this->listePompe(); //passe la methode avant de retourner la vue
+    
         // $this->listEmployer(); //passe la methode avant de retourner la vue
-        return view('livewire.ventes-create')->extends('layouts.app')->title('Creer une ventes');
+        return view('livewire.ventes-create',
+        [
+            // 'horaires' => $this->Config()
+        ])->extends('layouts.app')->title('Creer une ventes');
     }
 
 }
--- resources/views/livewire/ventes-create.blade.php
+++ resources/views/livewire/ventes-create.blade.php
@@ -1,15 +1,16 @@
 <div class="container mt-4">
     <h2>Feuille de routes de Pompistes</h2>
-    <form wire:submit.prevent="saveVente">
+    <form wire:submit.prevent="saveVentes">
         @csrf
         <div class="row">
             <div class="col-8">
                 <div class="form-row">
                     <div class="form-group col-12">
                         <label for="inputDate">Numero Pompe</label>
-                        <select name="mom_pompiste" class="form-control">
+                        <select wire:model="pompeId" name="mom_pompiste" class="form-control">
+                            <option class="bg-danger">Veuillez choisir</option>
                             @if ($liste_pompe->isEmpty())
-                                <option value="">Aucune pompe</option>
+                                <option>Aucune pompe</option>
                             @else
                                 @foreach ($liste_pompe as $pompe)
                                     <option value="{{ $pompe->id }}">{{ $pompe->nom }}</option>
@@ -21,49 +22,49 @@
                 <div class="form-row">
                     <div class="form-group col-6">
                         <label for="inputDate">Nom du Pompiste</label>
-                        <select name="mom_pompiste" class="form-control">
-                                @if ($users)
-                                    @foreach ($users as $user)
-                                        <option value="{{ $user->id }}">{{ $user->name }}</option>
-                                    @endforeach
-                                @endif
+                        <select wire:model="pompisteId" name="mom_pompiste" class="form-control">
+                            <option>Veuillez choisir</option>
+                            @if ($users)
+                                @foreach ($users as $user)
+                                    <option value="{{ $user->id }}">{{ $user->nom . ' ' . $user->prenom }}</option>
+                                @endforeach
+                            @else
+                                <option>Aucun employé</option>
+                            @endif
                         </select>
                     </div>
                     <div class="form-group col-6">
                         <label for="Heure">Heure de Service</label>
-                        <select name="heure_service" class="form-control">
+                        <select wire:model="heureService" name="a" class="form-control">
                             <option>Veuillez choisir ici</option>
-                            @if ($horaires)
-                                <option value="matin"> {{ $horaires['matin'] }} </option>
-                                <option value="soir"> {{ $horaires['soir'] }} </option>
-                            @else
-                                <option>Aucune heure n'a été ajoutée </option>
-                            @endif
+                            <option>{{ $horaires['matin'] }}</option>
+                            <option>{{ $horaires['soir'] }}</option>
                         </select>
-                    </div>                    
+                    </div>
                 </div>
                 <div class="form-row">
                     <div class="form-group col-6">
-                        <label for="">INDEX DEPART SUPER 91</label>
+
+                        <label for="">INDEX DEPART SUPER 91 | {{ $index_depart_essence }}</label>
                         <input type="number" name="index_depart_essence" class="form-control"
                             wire:model.live="index_depart_essence">
                     </div>
                     <div class="form-group col-6">
-                        <label for="">INDEX ARRIVEE SUPER 91</label>
+                        <label for="">INDEX ARRIVEE SUPER 91 | {{ $index_arrive_essence }}</label>
                         <input type="number" name="index_arrive_essence" class="form-control"
                             wire:model.live="index_arrive_essence">
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-group col-6">
-                        <label for="">INDEX DEPART GAZOIL </label>
+                        <label for="">INDEX DEPART GAZOIL | {{ $index_depart_gazoile }}</label>
                         <input type="number" name="index_depart_gazoile" class="form-control"
-                        wire:model.live="index_depart_gazoile">
+                            wire:model.live="index_depart_gazoile">
                     </div>
                     <div class="form-group col-6">
-                        <label for="">INDEX ARRIVEE GAZOIL</label>
+                        <label for="">INDEX ARRIVEE GAZOIL | {{ $index_arrive_gazoile }}</label>
                         <input type="number" name="index_arrive_gazoile" class="form-control"
-                        wire:model.live="index_arrive_gazoile">
+                            wire:model.live="index_arrive_gazoile">
                     </div>
                 </div>
                 <hr>
@@ -87,56 +88,65 @@
                             $prix_essence = 0;
                             if ($index_depart_essence != null && $index_arrive_essence != null) {
                                 $qte_essence = $index_arrive_essence - $index_depart_essence;
-                                $prix_essence = $qte_essence * 850;
+                                $prix_essence = $qte_essence * $unite_e;
                             }
                         @endphp
                         <label for="">Qté Super 91</label>
 
-                        <input class="form-control" value="{{ $qte_essence }}" readonly>
+                        <input wire:model="qte_essence" class="form-control" value="{{ $qte_essence }}" readonly>
                     </div>
                     <div class="form-group col">
                         <label for="">Prix Super 91</label>
-                        <input class="form-control" value="{{ $prix_essence }}" readonly>
+                        <input wire:model="prix_essence" class="form-control" value="{{ $prix_essence }}" readonly>
                     </div>
                 </div>
                 <div class="form-row rows-2">
                     <div class="form-group col">
-                    @php
-                        $qte_gazoile = 0;
-                        $prix_gazoile = 0;
-                        if ($index_depart_gazoile != null && $index_arrive_gazoile != null) {
-                            $qte_gazoile = $index_arrive_gazoile - $index_depart_gazoile;
-                            $prix_gazoile = $qte_essence * 600;
-                        }
-                    @endphp
+                        @php
+                            $qte_gazoile = 0;
+                            $prix_gazoile = 0;
+                            if ($index_depart_gazoile != null && $index_arrive_gazoile != null) {
+                                $qte_gazoile = $index_arrive_gazoile - $index_depart_gazoile;
+                                $prix_gazoile = $qte_gazoile * $unite_g;
+                            }
+                        @endphp
                         <label for="">Qté Gazoile</label>
-                        <input type="text" name="prix_gazoile" class="form-control"
-                        value="{{ $qte_gazoile }}" readonly>
+                        <input wire:model="prix_gazoile" type="text" name="prix_gazoile" class="form-control"
+                            value="{{ $qte_gazoile }}" readonly>
                     </div>
                     <div class="form-group col">
                         <label for="">Prix Gazoile</label>
-                        <input type="text" name="qte_gazoile" class="form-control"
-                        value="{{{ $prix_gazoile }}}" readonly>
+                        <input wire:model="qte_gazoile" type="text" name="qte_gazoile" class="form-control"
+                            value="{{ $prix_gazoile }}" readonly>
                     </div>
                 </div>
                 <div class="form-row rows-2">
+                    <div class="form-group col">
+                        {{ $prix_gazoile + $prix_essence }}
+                        <label for="">Total Montant</label>
+                        <input wire:model="montant" type="number" name="montant"
+                            value="{{ $prix_gazoile + $prix_essence }}" class="form-control" readonly>
+                    </div>
+                </div>
+                <div class="form-row rows-2">
+                    <div class="form-group col">
+                        <label for="">Montant recu</label>
+                        <input type="number" name="montant_recu" wire:model.live="montant_recu"
+                            class="form-control">
+                    </div>
+                    @php
+                        $ecart = 0;
+                        $montant = $prix_gazoile + $prix_essence;
+                        if ($montant_recu) {
+                            $ecart = $montant_recu - $montant;
+                            // dd($ecart);
+                        }
+                    @endphp
                     <div class="form-group col">
                         <label for="">Ecart ?</label>
-                        <input type="number" name="ecart" class="form-control" readonly>
-                    </div>
-                    <div class="form-group col">
-                        <label for="">Surplus ?</label>
-                        <input type="number" name="surplus" class="form-control"readonly>
-                    </div>
-                </div>
-                <div class="form-row rows-2">
-                    <div class="form-group col">
-                        <label for="">Total Montant</label>
-                        <input type="number" name="montant" class="form-control">
-                    </div>
-                    <div class="form-group col">
-                        <label for="">Montant recu </label>
-                        <input type="number" name="surplus" class="form-control">
+                        <input wire:model="ecart" type="number" value="{{ $ecart }}" name="ecart"
+                            class="form-control {{ $ecart >= 0 ? 'bg-success text-white' : 'bg-danger text-white' }}"
+                            readonly>
                     </div>
                 </div>
             </div>
